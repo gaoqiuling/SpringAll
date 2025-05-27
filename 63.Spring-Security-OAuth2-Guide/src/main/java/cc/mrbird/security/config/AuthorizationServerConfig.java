5979718a8be7dd24bd209f7ebd442eb20e820113
高秋玲 <gql@xinhuibaoli.com>
2025-05-27 09:28:15 +0800

    security guide 能跑得起来

--- 63.Spring-Security-OAuth2-Guide/src/main/java/cc/mrbird/security/config/AuthorizationServerConfig.java
+++ 63.Spring-Security-OAuth2-Guide/src/main/java/cc/mrbird/security/config/AuthorizationServerConfig.java
@@ -1,22 +1,83 @@
 package cc.mrbird.security.config;
 
+import cc.mrbird.security.service.UserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * @author MrBird
  */
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
+@Order(1)
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("test_key");
+        return accessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore jwtTokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("test")
+                .secret(passwordEncoder().encode("test1234"))
+                .authorizedGrantTypes("refresh_token", "authorization_code")
+                .accessTokenValiditySeconds(3600)
+                .refreshTokenValiditySeconds(86400)
+                .scopes("all")
+                .autoApprove(true)
+                .redirectUris("http://www.baidu.com");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("permitAll()")
+                .allowFormAuthenticationForClients()
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints
+                .tokenStore(jwtTokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter())
+                .authenticationManager(authenticationManager);
+    }
 }
+
--- 63.Spring-Security-OAuth2-Guide/src/main/java/cc/mrbird/security/config/ResourceServerConfig.java
+++ 63.Spring-Security-OAuth2-Guide/src/main/java/cc/mrbird/security/config/ResourceServerConfig.java
@@ -1,13 +1,26 @@
 package cc.mrbird.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * @author MrBird
  */
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig  {
+@Order(2)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .requestMatchers()
+            .antMatchers("/index/**")  // 只保护 /index/** 路径
+            .and()
+            .authorizeRequests()
+            .antMatchers("/index/**").authenticated();
+    }
 }
